23fca60a5fb1c46ca9d27d140fff20ea661c40cb
mwang840 <maxwang@udel.edu>
2022-06-14 16:23:00 -0400

    initial case of parkway

--- src/parkwayWalk.cpp
+++ src/parkwayWalk.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int parkwayWalk();
+int parkwayWalk(int m, int n);
 
 
 int main(){
+    int testCases, one, two;
+    cin >> testCases;
+    cout <<"\n";
+    for(int i = 0; i < testCases; ++i){
+        cin >> one >> two;
+        cout << parkwayWalk(one, two);
+    }
     return 0;
+}
+
+
+int parkwayWalk(int m, int n){
+    vector<int>theBench(n);
+    int distance = 0;
+    for(int i : theBench){
+        distance++;
+    }
+    return max(distance - m, 0);
 }
